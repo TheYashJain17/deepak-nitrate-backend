ac013041d8d1f1a70d2971906677976a0ea898a8
Yash Jain <yashjain07.yj@gmail.com>
2025-12-30 17:28:40 +0530

    Added The Controller To Send Data To VerifuClauseInclusion Of Our MicroService.

---
+++ controllers/clauseInclusion.controllers.ts
@@ -0,0 +1,54 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import errorResponse from "../src/utils/responses/error.response";
+import { VerifyClauseInclusionRequest, VerifyClauseInclusionResponse } from "../types/clauseInclusion.types";
+import clauseInclusionClient from "../src/grpc/clients/clauseInclusion.client";
+import grpc from "@grpc/grpc-js";
+import { getGrpcToHttpStatus } from "../src/utils/utilities/getHTTPStatusCode";
+import successResponse from "../src/utils/responses/success.response";
+
+export const clauseInclusionVerification = async(req: FastifyRequest, res: FastifyReply) => {
+
+    try {
+
+        const {agreementId, clauseSetHashId,commitment} = req.body as VerifyClauseInclusionRequest;
+
+        if(!agreementId || !clauseSetHashId || !commitment){
+
+            errorResponse(res, 400, "please provide agreementId, clauseSetHashId,commitment");
+            return;
+
+        }
+
+        const response = await new Promise((resolve, reject) => {
+
+            clauseInclusionClient.VerifyClauseInclusion({agreementId, clauseSetHashId, commitment}, (err: grpc.ServiceError, success: VerifyClauseInclusionResponse) => {
+
+                if(err){
+
+                    const httpCode = getGrpcToHttpStatus(err.code);
+
+                    errorResponse(res, httpCode, err.message);
+                    reject(err);
+                    return
+
+                }
+
+                console.log(`The data getting from microservice is from console is`, success);
+                resolve(success);
+
+
+            })
+        })
+
+        successResponse(res,200,response as VerifyClauseInclusionResponse);
+        
+    } catch (error) {
+
+        console.log(error);
+
+        errorResponse(res, 500,"Internal Server Error");
+        return;
+        
+    }
+
+}
