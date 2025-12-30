4b0f690406f8dc605b858d15bad51392f60e0d0a
Yash Jain <yashjain07.yj@gmail.com>
2025-12-30 16:57:23 +0530

    Create Server File For Exposing Created Service.

--- server.ts
+++ server.ts
@@ -1,1 +1,38 @@
-console.log("GoodBye world")
+import grpc from "@grpc/grpc-js";
+
+import protoLoader from "@grpc/proto-loader";
+import path from "path";
+
+import {fileURLToPath} from "url";
+import { ClauseInclusionServiceHandlers } from "./controllers/clauseInclusion.controllers";
+
+const __dirname = fileURLToPath(import.meta.url);
+
+const __filename = path.dirname(__dirname);
+
+
+const PROTO_PATH = path.join(__filename, "./proto/clauseInclusion.proto");
+
+const packageDef = protoLoader.loadSync(PROTO_PATH);
+
+const grpcObj = grpc.loadPackageDefinition(packageDef);
+
+const clauseInclusionPackage = grpcObj.clauseInclusion as any;
+
+
+const server = new grpc.Server();
+
+server.addService(clauseInclusionPackage.ClauseInclusionService.service, ClauseInclusionServiceHandlers);
+
+server.bindAsync("0.0.0.0:50051", grpc.ServerCredentials.createInsecure(), (err, port) => {
+
+    if(err){
+
+        console.log("The error we are getting from server",err);
+        return;
+
+    }
+
+    console.log(`Clause Inclusion Server Is Listening On ${port}`);
+
+})
