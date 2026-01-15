ce65aeb8fd3d0cb33f7b40a3518d64bd589998bf
Yash Jain <yashjain07.yj@gmail.com>
2026-01-16 00:16:15 +0530

    Fixed Some Issues.

--- api-gateway/src/routes/zkpVerification.routes.ts
+++ api-gateway/src/routes/zkpVerification.routes.ts
@@ -54,7 +54,7 @@ const zkpVerificationRoutes = async (fastify: FastifyInstance) => {
         "/clauseInclusion/addCommitment",
         addClauseInclusionCommitment
 
-    )
+    );
 
     fastify.post(
         "/clauseInclusion/verify",
